38bea4e76ea6fc8441b71f666300c104aa848530
Unknown <jeremie.chatillon@heig-vd.ch>
2017-10-27 09:32:47 +0200

    DirectedCycle
    Start création

--- DirectedCycle.h
+++ DirectedCycle.h
@@ -10,19 +10,38 @@
 #ifndef ASD2_DirectedCycle_h
 #define ASD2_DirectedCycle_h
 
+#include "ParcoursEnProfondeur.h"
+
 template<typename GraphType>
 class DirectedCycle {
 private:
 	/* A DEFINIR */
+    std::vector<bool> marked;
+    std::vector<bool> stacked;
+    bool cycleFinded = false;
+    const GraphType * g;
+        
 	
 public:
 	//constructeur
 	DirectedCycle(const GraphType& g) {
-		/* A IMPLEMENTER */
+            /* A IMPLEMENTER */
+            
+            marked.resize(g.V(), false);
+            stacked.resize(g.V(), false);
+            this->g = g;
+            
 	}
 	
 	//indique la presence d'un cycle
 	bool HasCycle() {
+            
+            
+            for(size_t i = 0; i < g.V(); ++i){
+                if(cycleFinded)
+                    return true;
+                else if(!marked())
+            }
 		/* A IMPLEMENTER */
 		//return ...
 	}
@@ -30,6 +49,7 @@ public:
 	//liste les indexes des sommets formant une boucle
 	std::list<int> Cycle() {
 		/* A IMPLEMENTER */
+                // Liste FIFO ajout 
 		//return ...
 	}
 	
---
+++ build/Debug/MinGW-Windows/main.o.d
@@ -0,0 +1,12 @@
+build/Debug/MinGW-Windows/main.o: main.cpp DiGraph.h TopologicalSort.h \
+ DirectedCycle.h Util.h SymbolGraph.h
+
+DiGraph.h:
+
+TopologicalSort.h:
+
+DirectedCycle.h:
+
+Util.h:
+
+SymbolGraph.h:
